a4138624c2e8092332b99e836d24a19c816e5695
dmitrivanich <dmitrivanich@gmail.com>
2022-02-17 19:47:55 +0300

    posible in the future will be added some sphere

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -8,11 +8,15 @@ import * as THREE from 'three';
 import "./Viewer.scss";
 
 function Scene() {
-  
-  const lines = useRef
-  const R = 10000
+  const linesVisable = true
+  const coordGridVisable = true
+
+
+  const R = 8000
   const pointSize = R/50
-  const coordGridSize = R/100
+  const coordLinesSize = R/100
+  const coordFlatSize = R*5
+
   const [activePoint, setActivePoint] = useState(0)
 
   const camerasTextures = [
@@ -20,11 +24,14 @@ function Scene() {
     '360-2.jpg'
   ];
 
-  const [Cx,Cz,Cy] = [0,-R,0] //расположение камеры
+  const coordsOfPoints = [
+    [0.01,-R,0.01],
+    [25000,-R,-4500],
+    [2000,-R, -20000]
+  ]
+  const [Cx,Cz,Cy] = coordsOfPoints[0] //расположение камеры
 
-
-
-  function createPoint([Ax,Ay,Az]) {
+  function getPointInfo([Ax,Az,Ay], index) {
     let AxyCz = ((Ay**2)+(Ax**2))**0.5
     let AxyC = ((R**2)+(AxyCz**2))**0.5
     let Kxy = Math.sign(Ay) * (R*AxyCz)/AxyC
@@ -32,24 +39,60 @@ function Scene() {
     let Ky = (Ay * Kxy)/AxyCz * Math.sign(Ay)
     let Kx = ((Kxy**2)-(Ky**2))**0.5 * Math.sign(Ax)
     let Kz = ((R**2)-(Kxy**2))**0.5 * -1
+    
+    return {
+      coords: [Kx,Kz,Ky],
+      angle: Math.acos(AxyCz/AxyC)
+    }
   }
-  const [Ax,Az,Ay] = [20000,-R,-5000]
 
+  function createPoint(coord,index){
+    function getPointColor(index){
+      const colors = ['#CCFF00','#CC6600','#FFCC00','#FF9999','#330000','#660099','#3300CC','#003366','#006666','#66CCFF','#339933','#999900'];
+      const randInd = Math.floor(Math.random()*(colors.length-1))
+      return(colors[randInd])
+    }
+    const pointColor = getPointColor(index)
+    return(
+      <group className={`point-${index}`} key={index}>
+              <mesh position={getPointInfo(coord,index).coords}
+                rotation={[Math.PI/2,getPointInfo(coord,index).angle,0]}>
+                <sphereBufferGeometry args={[pointSize,32]}/>
+                <meshBasicMaterial
+                  color={pointColor}
+                  side={THREE.DoubleSide}
+                  />
+              </mesh>
 
-  let AxyCz = ((Ay**2)+(Ax**2))**0.5
-  let AxyC = ((R**2)+(AxyCz**2))**0.5
-  let Kxy = Math.sign(Ay) * (R*AxyCz)/AxyC
-
-  let Ky = (Ay * Kxy)/AxyCz * Math.sign(Ay)
-  let Kx = ((Kxy**2)-(Ky**2))**0.5 * Math.sign(Ax)
-  let Kz = ((R**2)-(Kxy**2))**0.5 * -1
-
-
-  const pointsCoords = [
-        [[Cx,Cz,Cy],
-        [Ax,Az,Ay],
-        [Kx,Kz,Ky]]]
-
+              {coordGridVisable&&
+                <mesh position={coord}>
+                <sphereBufferGeometry args={[pointSize,50,50]} />
+                <meshBasicMaterial
+                  color={pointColor}
+                />
+                </mesh>
+              }
+              <group name="LINES">
+                <Line
+                    points={[[0, 0, 0],coord]}                                
+                    color="black"                  
+                    lineWidth={linesVisable? 0.5 : 0}
+                    dashed={true}   
+                  />
+                <Line
+                    points={[[0, -R, 0],coord]}                                
+                    color="grey"                  
+                    lineWidth={linesVisable? 1 : 0}               
+                  />
+                <Line
+                    points={[[0, 0, 0],getPointInfo(coord,index).coords]}                                
+                    color={pointColor}                  
+                    lineWidth={linesVisable? 1 : 0}                
+                  />
+              </group> 
+            </group>
+    )
+  }
 
   const texture = camerasTextures[activePoint]
   
@@ -58,34 +101,7 @@ function Scene() {
   sphereImg.wrapS = THREE.RepeatWrapping
   sphereImg.repeat.x = -1
   
-  function pointColor(index,activePoint){
-      if(index ===  activePoint){
-        return("black")
-      } else {
-        switch (index) {
-          case 0:
-            return "blue"
-            break;
-          case 1:
-            return "orange"
-            break;
-          case 2:
-            return "green"
-            break;
-          case 3:
-            return "yellow"
-            break;
-          case 4:
-            return "purple"
-            break;
-          
-          default:
-            return "red"
-            break;
-        }
-      }
-      
-  }
+  
   
   return (
     <>
@@ -94,89 +110,51 @@ function Scene() {
       <mesh position={[0,0,0]}
       
         rotation={[0,0.04,0]}>
-        <sphereBufferGeometry args={[R,100,100]}
-        
-        />
+        <sphereBufferGeometry args={[R,100,100]}/>
         <meshStandardMaterial
           transparent={true}
-          opacity={0.5}
+          opacity={1}
           depthTest={true}
           depthWrite={true}
           map={sphereImg}
-          side={THREE.DoubleSide}
-          />
+          side={THREE.DoubleSide}/>
       </mesh>
 
-      <mesh position={[0,-R,0]} rotation={[-Math.PI/2,0,0]}>
-        <circleBufferGeometry args={[R*10,32]}/>
-        <meshBasicMaterial
-          color={'#d3d3d3'}
-          side={THREE.DoubleSide}
-          />
-      </mesh>
 
-      <group ref={lines} name="LINES">
-        <Line
-            points={[[0, 0, 0],[Ax, Az, Ay]]}                                
-            color="black"                  
-            lineWidth={1} 
-            dashed={true}                  
-          />
-        <Line
-            points={[[0, Cz, 0],[Ax, Az, Ay]]}                                
-            color="#262277"                  
-            lineWidth={1} 
-            dashed={true}                  
-          />
-        <Line
-            points={[[0, 0, 0],[Cx, Cz, Cy]]}                                
-            color="red"                  
-            lineWidth={1} 
-            dashed={true}                  
-          />
-        <Line
-            points={[[Kx, 0, Ky],[Kx, Cz, Ky]]}                                
-            color="blue"                  
-            lineWidth={1} 
-            dashed={true}                  
-        />
-      </group>
-
-     <group name="COORDINAT_GRID"> 
-        <mesh position={[0,0,0]}>
-          <sphereBufferGeometry args={[pointSize,10,10]} />
-          <meshBasicMaterial
-            color={'white'}
-          />
-        </mesh>
-        <mesh position={[0,-R,0]}>
-          <boxBufferGeometry args={[coordGridSize,1,R*20]}/>
-          <meshBasicMaterial
-            color={'red'}
-          />
-        </mesh>
-        <mesh position={[0,-R,0]} rotation={[0,Math.PI/2,0]}>
-          <boxBufferGeometry args={[coordGridSize,1,R*20]}/>
-          <meshBasicMaterial
-            color={'blue'}
-          />
-        </mesh>
-     </group>
-
-     <group name="POINTS">
-      {/* ТОЧКИ ПЕРЕХОДА КАМЕРЫ */}
-      {pointsCoords[activePoint].map((coord, index)=>(
-        <mesh position={coord} key={index}
-        onClick={(e)=>{
-          setActivePoint(index)
-        }}>
-          <sphereBufferGeometry args={[pointSize,50,50]} />
-          <meshBasicMaterial
-            color={pointColor(index,activePoint)}
-          />
-        </mesh>
-      ))}
-     </group>
+     
+      {
+        coordsOfPoints.map((coord,index)=> createPoint(coord,index))
+      }
+ 
+     { coordGridVisable && 
+        <group name="COORDINAT_GRID"> 
+          <mesh position={[0,-R,0]} rotation={[-Math.PI/2,0,0]}>
+            <circleBufferGeometry args={[coordFlatSize,32]}/>
+            <meshBasicMaterial
+              color={'#d3d3d3'}
+              side={THREE.DoubleSide}
+              />
+          </mesh>
+          <mesh position={[0,0,0]}>
+            <sphereBufferGeometry args={[pointSize,10,10]} />
+            <meshBasicMaterial
+              color={'white'}
+            />
+          </mesh>
+          <mesh position={[0,-R,0]}>
+            <boxBufferGeometry args={[coordLinesSize,1,R*20]}/>
+            <meshBasicMaterial
+              color={'red'}
+            />
+          </mesh>
+          <mesh position={[0,-R,0]} rotation={[0,Math.PI/2,0]}>
+            <boxBufferGeometry args={[coordLinesSize,1,R*20]}/>
+            <meshBasicMaterial
+              color={'blue'}
+            />
+          </mesh>
+        </group>
+     }
     
     </>
   );
@@ -188,7 +166,7 @@ export default function App() {
       <div className="viewer">
         <Canvas linear camera={{
           filmGauge: 200,
-          position: [0,0,20000],
+          position: [0,0,100000],
           fov: 60,
           far: 100000000,
           near:10,
@@ -196,7 +174,6 @@ export default function App() {
           >
           <Suspense fallback={null}>
             <Scene />
-            
             <OrbitControls
               rotateSpeed={0.5}
               // enableZoom={false}
